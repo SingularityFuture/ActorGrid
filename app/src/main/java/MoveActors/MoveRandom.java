e0c33242e092b44ed247756538251a76e7cf0621
Michael <michael.mebane@gmail.com>
2017-01-31 15:20:01 -0500

    Add onClickListener to run button
    Set onClickListener to run simulation button.  Add first method to move random actor in new package.

---
+++ app/src/main/java/MoveActors/MoveRandom.java
@@ -0,0 +1,8 @@
+package MoveActors;
+
+/**
+ * Created by Michael on 1/31/2017.
+ */
+
+public class MoveRandom {
+}
--- app/src/main/java/com/example/michael/touchlab/MainActivity.java
+++ app/src/main/java/com/example/michael/touchlab/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.michael.touchlab;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button runButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addRunButtonListener();
     }
+
+    public void addRunButtonListener(){
+        runButton = (Button) findViewById(R.id.run_simulation);
+
+        runButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+    }
+
 }
