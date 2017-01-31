685e6a77109ddd627727a8c2f8d9ef4631a2ae49
Michael <michael.mebane@gmail.com>
2017-01-31 17:08:10 -0500

    Add ACTION_DONE effect to listeners
    Add listener for when user presses ENTER on the keyboard.

--- app/src/main/java/com/example/michael/touchlab/MainActivity.java
+++ app/src/main/java/com/example/michael/touchlab/MainActivity.java
@@ -2,44 +2,62 @@ package com.example.michael.touchlab;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button runButton;
-    private EditText actorText;
     public int gridRows;
     public int gridColumns;
     public String[] actors;
+    public int numberOfActors=R.integer.default_actors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        addActorTextListener();
+
+
+        LinearLayout layout = (LinearLayout)findViewById(R.id.activity_main);
+        for (int i = 0; i < layout.getChildCount(); i++) {
+            View v = layout.getChildAt(i);
+            if (v instanceof EditText && i<numberOfActors) {
+
+                addActorTextListener((EditText) v);
+            }
+            else if (v instanceof EditText && i>=numberOfActors) {
+
+                //addGridTextListener((EditText) v);
+            }
+        }
+
         addRunButtonListener();
 
-        actorText.performClick();
+        //actorText.performClick();
     }
 
-    public void addActorTextListener(){
-        actorText = (EditText) findViewById(R.id.actor_1);
+    public void addActorTextListener(final EditText actorText){
 
-
-
-        actorText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-
+        actorText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            boolean handled = false;
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                // Test listener with toast.
+                Toast.makeText(MainActivity.this, "Actor information entered", Toast.LENGTH_SHORT).show();
+                handled = true;
             }
+            return handled;
+        }
         });
-
-
-
     }
 
     public void addRunButtonListener(){
@@ -48,7 +66,8 @@ public class MainActivity extends AppCompatActivity {
         runButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                // Test listener with toast.
+                Toast.makeText(MainActivity.this, "Running Simulation", Toast.LENGTH_SHORT).show();
             }
         });
     }
