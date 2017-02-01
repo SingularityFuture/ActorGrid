0091ecd398e79b66f5f99a8137a4275b0090cbc5
Michael Mebane <michael.mebane@gmail.com>
2017-02-01 09:47:51 -0500

    Add listener to number of frames EditText
    Add comments to MainActivity.  Add EditText for number of frames and its listener.

--- app/src/main/java/com/example/michael/touchlab/MainActivity.java
+++ app/src/main/java/com/example/michael/touchlab/MainActivity.java
@@ -18,12 +18,11 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button runButton;
-    public int gridRows;
-    public int gridColumns;
-    public List<String> actors = new ArrayList<String>();
-    public int numberOfActors;
-    //public int numberOfActors=getResources().getInteger(R.integer.default_actors);
+    private Button runButton; // Button that runs the simulation
+    public int gridRows; // Number of rows on grid
+    public int gridColumns; // Number of columns on grid
+    public List<String> actors = new ArrayList<String>(); // Create list for all actors' traits
+    public int numberOfActors; // Number of actors
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +30,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         numberOfActors=getResources().getInteger(R.integer.default_actors); // Set the initial # of actors
 
-        LinearLayout layout = (LinearLayout)findViewById(R.id.activity_main);
-        for (int i = 1; i < layout.getChildCount(); i++) {
-            View v = layout.getChildAt(i);
-            if (v instanceof EditText && i<=numberOfActors) {
-                actors.add(getResources().getStringArray(R.array.actor_array)[i]); // Set the initial actor traits
-                addActorTextListener((EditText) v);
+        LinearLayout layout = (LinearLayout)findViewById(R.id.activity_main); // Get the main activity
+        for (int i = 1; i < layout.getChildCount(); i++) { // For each element in the layout, starting after the first TextView
+            View v = layout.getChildAt(i); // Get each child
+            if (v instanceof EditText && i<=numberOfActors) { // If this is an EditText element and its within the actor section
+                actors.add(getResources().getStringArray(R.array.actor_array)[i-1]); // Set the initial actor traits
+                addActorTextListener((EditText) v); // Add a listener to the EditText view if the input changes
             }
-            else if (v instanceof EditText && i>numberOfActors) {
-                addGridRowColumnListener((EditText) v);
+            else if (v instanceof EditText && i>numberOfActors && i<=numberOfActors+2) { // If its in the grid row and column section,
+                addGridRowColumnListener((EditText) v); // Then add a listener for that
+            }
+            else if (v instanceof EditText && i==numberOfActors+3) { // If this is the frame input
+                addFrameListener((EditText) v); // Then add a listener for that
             }
         }
         addRunButtonListener();
@@ -53,10 +55,9 @@ public class MainActivity extends AppCompatActivity {
         public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
             boolean handled = false;
             if (actionId == EditorInfo.IME_ACTION_DONE) {
-                // Test listener with toast.
-                Toast.makeText(MainActivity.this, "Actor information entered", Toast.LENGTH_SHORT).show();
-                InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE); // Hide keyboard
-                mgr.hideSoftInputFromWindow(actorText.getWindowToken(), 0);
+                Toast.makeText(MainActivity.this, "Actor information entered", Toast.LENGTH_SHORT).show(); // Test listener with toast.
+                InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE); // Get the keyboard
+                mgr.hideSoftInputFromWindow(actorText.getWindowToken(), 0); // Get the keyboard
 
                 handled = true;
             }
@@ -72,10 +73,27 @@ public class MainActivity extends AppCompatActivity {
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 boolean handled = false;
                 if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    // Test listener with toast.
-                    Toast.makeText(MainActivity.this, "Grid information entered", Toast.LENGTH_SHORT).show();
-                    InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE); // Hide keyboard
-                    mgr.hideSoftInputFromWindow(gridText.getWindowToken(), 0);
+                    Toast.makeText(MainActivity.this, "Grid information entered", Toast.LENGTH_SHORT).show(); // Test listener with toast.
+                    InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE); // Get the keyboard
+                    mgr.hideSoftInputFromWindow(gridText.getWindowToken(), 0); // Hide the keyboard
+
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+    }
+
+    public void addFrameListener(final EditText frameText){
+
+        frameText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    Toast.makeText(MainActivity.this, "Frame information entered", Toast.LENGTH_SHORT).show(); // Test listener with toast.
+                    InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE); // Get the keyboard
+                    mgr.hideSoftInputFromWindow(frameText.getWindowToken(), 0); // Hide keyboard
 
                     handled = true;
                 }
